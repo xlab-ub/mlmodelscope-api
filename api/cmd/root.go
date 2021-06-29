fe553f1452d90cc4336d6f31199145e628cecce6
Ben Young <ben@pixotech.com>
2021-06-29 10:59:18 -0500

    Add tracing to API /predict/* endpoints
    Also add a Dockerfile that supports debugging with Delve

--- api/cmd/root.go
+++ api/cmd/root.go
@@ -1,17 +1,27 @@
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"github.com/apex/log"
+	"github.com/c3sr/config"
 	dlframework "github.com/c3sr/dlframework/http"
 	"github.com/c3sr/dlframework/httpapi/restapi"
 	"github.com/c3sr/dlframework/httpapi/restapi/operations"
+	"github.com/c3sr/tracer"
+	tracermiddleware "github.com/c3sr/tracer/middleware"
+	"github.com/c3sr/uuid"
 	"github.com/go-openapi/loads"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os/signal"
+	"time"
+
+	"fmt"
 	"net/http"
 	"os"
-
-	"github.com/c3sr/config"
-	"github.com/spf13/cobra"
+	"strings"
 )
 
 var rootCmd = &cobra.Command{
@@ -25,18 +35,58 @@ const DefaultPort = "80"
 
 func serve(cmd *cobra.Command, args []string) {
 	port := viper.Get("Port")
-
+	e := echo.New()
 	handler, err := getDlframeworkHandler()
+
 	if err != nil {
 		panic(fmt.Errorf("unable to create handler: %s", err))
 	}
 
-	http.Handle("/", handler)
-	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
-	if err != nil {
-		panic(fmt.Errorf("unable to listen: %s", err))
+	configureEcho(e)
+	addRoutes(e, handler)
+
+	go func() {
+		err = e.Start(fmt.Sprintf(":%s", port))
+		if err != nil {
+			panic(fmt.Errorf("unable to listen: %s", err))
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		log.WithError(err).Fatal("Failed to gracefully shutdown server")
 	}
 }
+
+func configureEcho(e *echo.Echo) {
+	e.Use(middleware.Recover())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: middleware.DefaultLoggerConfig.Format,
+	}))
+	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{
+		Generator: func() string {
+			return uuid.NewV4()
+		},
+	}))
+	e.Use(AllowTracedHeaders())
+}
+
+func addRoutes(e *echo.Echo, handler http.Handler) {
+	api := e.Group("/api")
+	api.Any("/predict*",
+		echo.WrapHandler(handler),
+		tracermiddleware.FromHTTPRequest(tracer.Std(), "api_request"),
+		tracermiddleware.ToHTTPResponse(tracer.Std()),
+	)
+	api.Any("/*", echo.WrapHandler(handler))
+}
+
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -55,8 +105,14 @@ func init() {
 	viper.SetEnvPrefix("c3sr")
 	viper.BindEnv("Port", "port")
 	viper.SetDefault("Port", DefaultPort)
+	viper.BindEnv("app.name", "app_name")
+	viper.SetDefault("app.name", "carml")
 	viper.BindEnv("registry.provider", "registry_provider")
 	viper.BindEnv("registry.endpoints", "registry_endpoints")
+	viper.BindEnv("tracer.enabled", "tracer_enabled")
+	viper.BindEnv("tracer.provider", "tracer_provider")
+	viper.BindEnv("tracer.endpoints", "tracer_endpoints")
+	viper.BindEnv("tracer.level", "tracer_level")
 	viper.AutomaticEnv()
 }
 
@@ -70,4 +126,32 @@ func getDlframeworkHandler() (http.Handler, error) {
 	handler := dlframework.ConfigureAPI(api)
 
 	return handler, nil
+}
+
+func AllowTracedHeaders() echo.MiddlewareFunc {
+	headers := strings.Join(
+		[]string{
+			"Origin",
+			"Accept",
+			"X-Requested-With",
+			"X-B3-TraceId",
+			"X-B3-ParentSpanId",
+			"X-B3-SpanId",
+			"X-B3-Sampled",
+			"trace.traceid",
+			"trace.spanid",
+		},
+		", ")
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			res := c.Response()
+			rid := req.Header.Get(echo.HeaderAccessControlAllowHeaders)
+			rid += headers
+			res.Header().Set(echo.HeaderAccessControlAllowHeaders, rid)
+
+			return next(c)
+		}
+	}
 }
--- api/go.mod
+++ api/go.mod
@@ -5,9 +5,14 @@ go 1.15
 replace google.golang.org/grpc => google.golang.org/grpc v1.29.1
 
 require (
+	github.com/apex/log v1.9.0
 	github.com/c3sr/config v1.0.1
 	github.com/c3sr/dlframework v1.3.4
+	github.com/c3sr/tracer v1.0.4
+	github.com/c3sr/uuid v1.0.1
 	github.com/go-openapi/loads v0.20.2
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.2.1
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 )
