2855f89f9a211eee98dbbfc8b49425de35eb9ea0
Ben Young <ben@pixotech.com>
2021-11-16 10:32:55 -0600

    Add version number and optional registry parameters to container build script

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -3,15 +3,22 @@
 cd `git rev-parse --show-toplevel`
 source "scripts/functions.sh"
 
-USAGE="Usage: ${0} <target>
+USAGE="Usage: ${0} <target> <version> (registry)
 
-    target        The service to build into a container, i.e. 'uploader':
-                      > build.sh uploader"
+    target        (required) The service to build into a container, i.e. 'uploader':
+
+    version       (required) The version number to tag the image with
+                      Note: image will always be tagged as 'latest'
+
+    registry      (optional) The Docker registry to tag images against
+                      Defaults to 'mlmodelscope-api'"
 
 TARGET="$1"
 TARGET_DIR="./$TARGET"
+VERSION="$2"
+REGISTRY="$3"
+: ${REGISTRY:="mlmodelscope-api"}
 DOCKERFILE="./docker/Dockerfile.$1"
-
 
 if [ -z $TARGET ]; then
     die_with_message "$USAGE"
@@ -21,10 +28,15 @@ if [ ! -d "$TARGET" ]; then
     die_with_message "Target ${TARGET} does not exist"
 fi
 
+if [ -z $VERSION ]; then
+    die_with_message "$USAGE"
+fi
+
 if [ ! -f "$DOCKERFILE" ]; then
     die_with_message "Dockerfile ${DOCKERFILE} does not exist"
 fi
 
-docker build -t "mlmodelscope-api/$1:latest" --file "$DOCKERFILE" "$TARGET"
+docker build -t "$REGISTRY/mlmodelscope-$TARGET:$VERSION" --file "$DOCKERFILE" "$TARGET_DIR"
+docker tag "$REGISTRY/mlmodelscope-$TARGET:$VERSION" "$REGISTRY/$TARGET:latest"
 
 cleanup_and_exit
